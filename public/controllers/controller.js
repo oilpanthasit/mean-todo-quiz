44456a06943ad8d881c8bb360458dadc1e9c547c
oil\Panthasit <oilzayo22820@gmail.com>
2017-01-05 14:39:11 +0700

    complete main feature

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,52 +1,51 @@
-var myApp = angular
-    .module('myApp', [])
-    .controller('AppCtrl', function($scope, $http) {
+var myApp = angular.module('myApp', [
+    'ngRoute']).
+    config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/todopage', { templateUrl: 'todopage.html' });
+        $routeProvider.when('/settingpage', { templateUrl: 'settingpage.html' });
+        $routeProvider.otherwise({ redirectTo: '/index.html' });
 
-
-        var refresh = function() {
-            $http.get("http://45.32.114.176:4600/todos")
-                .then(function(response) {
-                    var data = response.data.todos;
-                    $scope.lists = data;
-                    $scope.title = '';
-                });
-        };
-
-        refresh();
-
-        $scope.addList = function() {
-            var title = $scope.title;
-            axios.post('http://45.32.114.176:4600/todos', { todo: title })
-                .then(function(response) {
-                    console.log(response);
-                    refresh();
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
-        };
-
-        $scope.remove = function(id) {
-            console.log(id);
-            axios.delete('http://45.32.114.176:4600/todos/'+id)
-                .then(function(response) {
-                    console.log(response);
-                    refresh();
-                })
-                .catch(function(response) {
-                    console.log(response);
-                });
-        };
+        $locationProvider.html5Mode({ enabled: true, requireBase: false });
+    }]);
 
 
 
-    });
+myApp.controller('AppCtrl', function ($scope, $http) {
+
+
+    var refresh = function () {
+        $http.get("http://45.32.114.176:4600/todos")
+            .then(function (response) {
+                var data = response.data.todos;
+                $scope.lists = data;
+                $scope.title = '';
+            });
+    };
+
+    refresh();
+
+    $scope.addList = function () {
+        var title = $scope.title;
+        axios.post('http://45.32.114.176:4600/todos', { todo: title })
+            .then(function (response) {
+                refresh();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    $scope.remove = function (id) {
+        console.log(id);
+        axios.delete('http://45.32.114.176:4600/todos/' + id)
+            .then(function (response) {
+                refresh();
+            })
+            .catch(function (response) {
+                console.log(response);
+            });
+    };
 
 
 
-
-
-
-
-
-
+});
